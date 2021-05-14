7d42e03a33758971cf4993692ae12e663378fe4d
Jean-1220 <123sijin@sookmyung.ac.kr>
2021-05-14 15:30:23 +0900

    study python

---
+++ practice_02_01_02.py
@@ -0,0 +1,13 @@
+#연산자_연산자_02논리
+
+print(1 != 3) # 같지않다 True
+print(not(1 != 3)) # False
+
+print((3 > 0) and (3 < 5)) # True
+print((3 > 0) & (3 <5)) # True
+
+print((3 > 0) or (3 > 5)) # True
+print((3 > 0) | (3 >5)) # True
+
+print(5 > 4 > 3) # True
+print(5 > 4 > 7) #False
